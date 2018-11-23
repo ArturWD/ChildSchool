33f9c06ac916c4d4aae581ad3b9bf1f49f6cd6f7
Artur Shestakov <shestakovartur2.0@gmail.com>
2018-11-23 13:45:58 +0500

    ready

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,11 @@
 $(function() {
+	// services
+	$(".about-img").imagefill();
+	$(".service-img").imagefill();
+	//NEWS
+	$('.news-preview').imagefill();
+	
+	//MENU
 	$(window).resize(function(){		
 		if( $(window).width() < 900 ){
 			
@@ -13,13 +20,43 @@ $(function() {
 		}
 		
 	});
+	
+	$(window).scroll(function(){
+		if( $(window).width() > 900 ){
+			$('.drop-down-menu').slideUp(300, function(){});
+		}
+	});
+	
 	$(".navigation--hamburger").on('click', function(){
 		$('.navigation--menu').slideToggle(300, function(){});
+		
 	});
 
 	$('.drop-down-item').on('click', function(e){
-		if( $(window).width() < 900 ){
+		
 			$(e.currentTarget).find('.drop-down-menu').slideToggle(300, function(){});	
+		
+	});
+	
+	if( $(window).width() < 900 ){
+		$('.navigation--menu').slideUp(0, function(){});
+	};
+	
+	//REVIEWS
+	$('.reviews-carousel').owlCarousel({
+		items:1,
+		loop:true,
+		responsiveClass:true,
+		margin:30,
+		responsive:{
+			500:{
+				items: 2
+			},
+			800:{
+				items:3
+			}
 		}
 	});
+	
 });
+
